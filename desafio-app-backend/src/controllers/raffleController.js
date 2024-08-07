983ddf55f080d2f2be1c8587e531f4a5bfe5cf55
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-07 19:00:20 -0300

    feature: Implement method to list all raffles

--- desafio-app-backend/src/controllers/raffleController.js
+++ desafio-app-backend/src/controllers/raffleController.js
@@ -1,16 +1,4 @@
-const { Raffle } = require('../models/Raffle');
-
-
-// Método para gerar um número único para a rifa de sorteio
-async function generateUniqueNumber() {
-    let number;
-    let exists = true;
-    while (exists) {
-        number = Math.floor(Math.random() * 1000000);
-        exists = await Raffle.findOne({where: {number} });
-    }
-    return number;
-}
+const {Raffle} = require('../models/Raffle');
 
 // Método para comprar uma rifa de sorteio
 exports.buyRaffle = async (req, res) => {
@@ -18,12 +6,13 @@ exports.buyRaffle = async (req, res) => {
         const {user} = req.body;
         const number = await generateUniqueNumber();
 
-        const newRaffle = await new Raffle({number, purchased: true, user});
+        const newRaffle = await Raffle.create({number, purchased: true, user});
         res.status(201).json({
             message: 'Rifa comprada com sucesso', raffle: newRaffle
         });
     }
     catch (error) {
+        console.error('Erro ao comprar rifa:', error);
         res.status(500).json({
             message: 'Error ao comprar rifa', error
         })
@@ -45,8 +34,33 @@ exports.drawRaffle = async (req, res) => {
             message: 'Sorteio realizado com sucesso', winner
         });
     } catch (error) {
+        console.error('Erro ao comprar rifa:', error);
         res.status(500).json({
             message: 'Erro ao realizar o sorteio', error
         });
     }
+}
+
+// Método para listar todas as rifas
+exports.getAllRaffles = async (req, res) => {
+    try {
+        const raffles = await Raffle.findAll();
+        res.status(200).json(raffles);
+    } catch (error) {
+        console.error('Erro ao comprar rifa:', error);
+        res.status(500).json({
+            message: 'Erro ao buscar rifas', error
+        });
+    }
+}
+
+// Método para gerar um número único para a rifa de sorteio
+async function generateUniqueNumber() {
+    let number;
+    let exists = true;
+    while (exists) {
+        number = Math.floor(Math.random() * 1000000);
+        exists = await Raffle.findOne({where: {number} });
+    }
+    return number;
 }
