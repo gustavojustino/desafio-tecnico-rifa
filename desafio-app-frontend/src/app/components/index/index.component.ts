050b4804c1701fb453d49f681d87db0e906f86ad
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-09 12:18:56 -0300

    refactor: implement decoupling to buy raffle component

--- desafio-app-frontend/src/app/components/index/index.component.ts
+++ desafio-app-frontend/src/app/components/index/index.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../layouts/menu/menu.component";
 import { FooterComponent } from '../layouts/footer/footer.component';
-import { RaffleComponent } from "../raffle/raffle.component";
+import { BuyRaffleComponent } from '../raffle/buy-raffle/buy-raffle.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [MenuComponent, FooterComponent, RaffleComponent],
+  imports: [MenuComponent, FooterComponent, BuyRaffleComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
